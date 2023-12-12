8e5230dcd2893b80aa2108419953baea783c38c4
Aulona <aulonahaliti98@gmail.com>
2023-12-12 15:21:22 +0100

    Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark");
+    var logo = document.querySelector('.dode');
+    logo.classList.toggle('dode-dark');
+    var rrejte = document.querySelector(".ic");
+    rrejte.classList.toggle('icd');
+}
